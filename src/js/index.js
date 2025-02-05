1809fbb6af7552c19de6f51e044b72fbe044e601
Leonid <zemlyanykh@bk.ru>
2025-02-05 11:54:43 +0500

    Project init

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,7 @@
 import css from "../css/main.css"
-import { Popovers } from "./popovers/popover";
 
 console.log('JS Init')
 
 document.addEventListener("DOMContentLoaded", function () {
-    const popover = new Popovers(document.querySelector('.main'));
-    popover.buildPopover();
+    
 });
